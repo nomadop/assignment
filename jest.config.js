4a9959f87e8b3a31f3ccbbe48b79f2b2f7a28f9f
Longju Cheng <nomadop@gmail.com>
2023-10-30 21:52:33 +0800

    init for test

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  preset: 'jest-expo',
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
+  moduleFileExtensions: [
+    'ts',
+    'tsx',
+    'js',
+    'jsx',
+    'json',
+    'node',
+  ],
+  setupFilesAfterEnv: [
+    '@testing-library/jest-native/extend-expect',
+  ],
+};
